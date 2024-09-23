a6444209da5e334633034286a3d1cb8ece42959b
Naiiky <dauvergne.hedy@gmail.com>
2024-09-23 15:32:46 +0200

    Js expressions added to the User and button

--- src/User.jsx
+++ src/User.jsx
@@ -1,8 +1,15 @@
 const User = () => {
+  const myName = "Hedy Dauvergne";
+  const myAge = 26;
+  const myCountry = "France";
+
   return (
     <>
-      <h1>Hello my name is Hedy Dauvergne</h1>
-      <p>I am a student from France, I love coding and I am 26 year old.</p>
+      <h1>Hello my name is {myName}</h1>
+      <p>
+        I am a student from {myCountry}, I love coding and I am {myAge} year
+        old.
+      </p>
       <p>I am developping a website with React</p>
     </>
   );
