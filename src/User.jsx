532191451d43802b3e4a044ffc6a172aa901ed29
Naiiky <dauvergne.hedy@gmail.com>
2024-09-23 14:47:36 +0200

    first commit

---
+++ src/User.jsx
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <>
+      <h1>Hello my name is Hedy Dauvergne</h1>
+      <p>I am a student from France, I love coding and I am 26 year old.</p>
+      <p>I am developping a website with React</p>
+    </>
+  );
+};
+
+export default User;
