588ed831be57aac79c982e111ee2e9f514d54f61
Naiiky <dauvergne.hedy@gmail.com>
2024-09-24 14:49:27 +0200

    Use State on button

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,28 +1,22 @@
 import { useState } from "react";
 
 const Button = () => {
-  // Déclaration du state 'count' initialisé à 0
   const [count, setCount] = useState(0);
 
-  // Fonction pour incrémenter le state de 5
   const incrementCount = () => {
     setCount(count + 5);
   };
 
-  // Fonction pour décrémenter le state de 5
   const decrementCount = () => {
     setCount(count - 5);
   };
 
   return (
     <div>
-      {/* Affichage de la valeur actuelle de count */}
       <h1>Count: {count}</h1>
 
-      {/* Bouton pour incrémenter de 5 */}
       <button onClick={incrementCount}>Incrémenter de 5</button>
 
-      {/* Bouton pour décrémenter de 5 */}
       <button onClick={decrementCount}>Décrémenter de 5</button>
     </div>
   );
